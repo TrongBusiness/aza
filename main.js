bf7eff00a39ba342e6638398131a88851e3896ca
Trong Van <trongvan214@yahoo.com>
2018-03-07 16:43:51 -0600

    update

--- main.js
+++ main.js
@@ -1,360 +1,105 @@
-const qs = document.querySelector.bind(document);
-const easingHeart = mojs.easing.path('M0,100C2.9,86.7,33.6-7.3,46-7.3s15.2,22.7,26,22.7S89,0,100,0');
+window.onload = function(argument) {
 
-const el = {
-  container: qs('.mo-container'),
-  
-  i: qs('.lttr--I'),
-  l: qs('.lttr--L'),
-  o: qs('.lttr--O'),
-  v: qs('.lttr--V'),
-  e: qs('.lttr--E'),
-  y: qs('.lttr--Y'),
-  o2: qs('.lttr--O2'),
-  u: qs('.lttr--U'),
-  
-  lineLeft: qs('.line--left'),
-  lineRight: qs('.line--rght'),
-  
-  colTxt: "#763c8c",
-  colHeart: "#fa4843",
-  
-  blup: qs('.blup'),
-  blop: qs('.blop'),
-  sound: qs('.sound')
-};
+	// var lyric = "i couldn't take it couldn't stand another minute couldn't bear another day without you in it";
+  var lyric = `Attractive Bright Brilliant Wonderful Splendid Delightful Elegant Exquisite Glorious Lavish Lovely Pleasing Stunning Plush Shimmering
+              Graceful Elegant Cher Pretty Cute Charming Angelic Divine Marvelous Marvelous Beautiful Dainty Captivating Classy Bewitching Gorgeous
+              Graceful Elegant Cher Foxy Pretty Cute Charming Angelic Divine Marvelous Marvelous Beautiful Dainty Captivating Classy Bewitching Gorgeous
+              Magnetic Mesmeric Teasing Hottie Adorable Foxy Amazing Radiant Sexy Hot Appealing Alluring Beguiling Breathtaking Dainty Dreamy Fabulous Goddess
+              Heart-stopping Irresistible Hypnotic Graceful Luscious Perfect Smoking Spectacular Striking Special Affectionate Dependable Compassionate Patient
+              Irreplaceable Fantastic Courageous Smart Awesome Determined Fun Delightful Thoughtful Thicc Respectful Cute Blessing Loving Seductive Magical
+              Sassy Feisty Sincere Funny Spontaneous Cheeky Naughty Unique Precious Wise Clever Joyful Kind Quirky Witty Rare Yummy Likable Breathtaking Babe Enchanting 
+              Attractive Bright Brilliant Wonderful Splendid Delightful Elegant Exquisite Glorious Lovely Pleasing Stunning Plush Shimmering
+              Attractive Bright Brilliant Wonderful Splendid Delightful Elegant Exquisite Glorious Lavish Lovely Pleasing Stunning Plush Shimmering
+              Graceful Elegant Cher Foxy Pretty Cute Charming Angelic Divine Marvelous Marvelous Beautiful Dainty Captivating Classy Bewitching Gorgeous
+              Magnetic Mesmeric Teasing Hottie Adorable Foxy Amazing Radiant Sexy Hot Appealing Alluring Beguiling Breathtaking Dainty Dreamy Fabulous Goddess
+              Heart-stopping Irresistible Hypnotic Graceful Luscious Perfect Smoking Spectacular Striking Special Affectionate Dependable Compassionate Patient
+              Irreplaceable Fantastic Courageous Smart Awesome Determined Fun Delightful Thoughtful Thicc Respectful Cute Blessing Loving Seductive Magical
+              Irreplaceable Fantastic Courageous Smart Awesome Determined Fun Delightful Thoughtful Thicc Respectful Cute Blessing Loving Seductive Magical
+              Irreplaceable Fantastic Courageous Smart Awesome Determined Fun Delightful Thoughtful Thicc Respectful Cute Blessing Loving Seductive Magical
+              Sassy Feisty Sincere Funny Spontaneous Cheeky Naughty Unique Precious Wise Clever Joyful Kind Quirky Witty Rare Yummy Likable Breathtaking Babe Enchanting 
+              Extraordinary Gifted Inspiring Intoxicating Playful Remarkable Provocative Queen Sugar Talented Unselfish Vulnerable Lovely Lovely Lovely
+              Aza Aza Aza Aza Aza Aza Aza Aza Aza Aza Aza Aza Aza Aza Aza Aza Aza Aza Aza Aza Aza Aza Aza Aza Aza Aza
+              Hi Hi Hi Hi Hi Hi Hi Hi Hi Hi Hi Hi Hi Hi Hi Hi Hi Hi Hi Hi Hi Hi Hi Hi Hi Hi Hi Hi`;
+	var words = {};
+	var words_attr = [];
+	string_handle(lyric);
+  //2636
+	var canvas = document.getElementById('c');
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
 
-class Heart extends mojs.CustomShape {
-  getShape() {
-    return '<path d="M50,88.9C25.5,78.2,0.5,54.4,3.8,31.1S41.3,1.8,50,29.9c8.7-28.2,42.8-22.2,46.2,1.2S74.5,78.2,50,88.9z"/>';
-  }
-  getLength () { return 200; }
+	if (canvas.getContext) {
+		var c = canvas.getContext('2d'),
+			w = canvas.width,
+			h = canvas.height;
+
+		c.strokeStyle = 'red';
+		c.fillStyle = 'white';
+		c.lineWidth = 5;
+
+		// constructor
+		Word = function(key) {
+			this.text = key;
+			this.x = Math.random() * w;
+			this.y = Math.random() * h;
+			this.font = words[key] * 10 + 'px arial'
+			this.speed = (words[key]);
+		}
+		for (key in words) {
+			words_attr.push(new Word(key));
+		}
+		console.log(words_attr.length);
+
+		function animation() {
+			for (var i = 0; i < words_attr.length; i++) {
+				c.font = words_attr[i].font;
+				c.fillText(words_attr[i].text, words_attr[i].x, words_attr[i].y);
+				words_attr[i].width = c.measureText(words_attr[i].text).width;
+				c.stroke();
+			}
+			move();
+		}
+
+		function move() {
+			for (var i = 0; i < words_attr.length; i++) {
+				if (words_attr[i].x > w) {
+					words_attr[i].x = -words_attr[i].width;
+					words_attr[i].y = Math.random()*h;
+				}else{
+					words_attr[i].x += words_attr[i].speed;
+				}
+			}
+		}
+
+		setInterval(function() {
+			c.clearRect(0,0,w,h);
+			animation();
+		},24);
+
+	}
+
+	function string_handle(str) {
+		var split_str = str.split(" ");
+		var word_array = [];
+		var word_count = [];
+		for (var i = 0; i < split_str.length; i++) {
+			check = true;
+			for (var j = 0; j <= word_array.length; j++) {
+				if (split_str[i] == word_array[j]) {
+					word_count[j]++;
+					check = false;
+					break;
+				}
+			}
+			if (check) {
+				word_array.push(split_str[i]);
+				word_count.push(1);
+			}
+		}
+		for (var i = 0; i < word_array.length; i++) {
+			words[word_array[i]] = word_count[i];
+		}
+		return words;
+	}
+
 }
-mojs.addShape('heart', Heart);
-
-const crtBoom = (delay = 0, x = 0, rd = 46) => {
-  parent = el.container;
-  const crcl = new mojs.Shape({
-    shape:        'circle',
-    fill:         'none',
-    stroke:        el.colTxt,
-    strokeWidth:  { 5 : 0 },
-    radius:       { [rd] : [rd + 20] },
-    easing:       'quint.out',
-    duration:     500 / 3,
-    parent,
-    delay,
-    x
-  });
-  
-  const brst = new mojs.Burst({
-    radius:       { [rd + 15] : 110 },
-    angle:        'rand(60, 180)',
-    count:        3,
-    timeline:     { delay },
-    parent,
-    x,
-    children: {
-      radius:       [5, 3, 7],
-      fill:         el.colTxt,
-      scale:        { 1: 0, easing: 'quad.in' },
-      pathScale:    [ .8, null ],
-      degreeShift:  [ 'rand(13, 60)', null ],
-      duration:     1000 / 3,
-      easing:       'quint.out'
-    }
-  });
-  
-  return [crcl, brst];
-};
-
-const crtLoveTl = () => {
-  const move        = 1000;
-  const boom        = 200;
-  const easing      = 'sin.inOut';
-  const easingBoom  = 'sin.in';
-  const easingOut   = 'sin.out';
-  const opts        = { duration: move, easing, opacity: 1 };
-  const delta       = 150;
-  
-  return (new mojs.Timeline).add([
-    new mojs.Tween({
-      duration: move,
-      onStart: () => {
-        [el.i, el.l, el.o, el.v, el.e, el.y, el.o2, el.u].forEach(el => {
-          el.style.opacity = 1;
-          el.style = 'transform: translate(0px, 0px) rotate(0deg) skew(0deg, 0deg) scale(1, 1); opacity: 1;'
-        })
-      },
-      onComplete: () => {
-        [el.l, el.o, el.v, el.e].forEach(el => el.style.opacity = 0);
-        el.blop.play();
-      }
-    }),
-    
-    new mojs.Tween({
-      duration: move * 2 + boom,
-      onComplete: () => {
-        [el.y, el.o2].forEach(el => el.style.opacity = 0);
-        el.blop.play();
-      }
-    }),
-  
-    new mojs.Tween({
-      duration: move * 3 + boom * 2 - delta,
-      onComplete: () => { 
-        el.i.style.opacity = 0;
-        el.blop.play();
-      }
-    }),
-  
-    new mojs.Tween({
-      duration: move * 3 + boom * 2,
-      onComplete: () => { 
-        el.u.style.opacity = 0; 
-        el.blup.play();
-      }
-    }),
-  
-    new mojs.Tween({
-      duration: 50,
-      delay: 4050,
-      onUpdate: (progress) => {
-        [el.i, el.l, el.o, el.v, el.e, el.y, el.o2, el.u].forEach(el => {
-          el.style = `transform: translate(0px, 0px) rotate(0deg) skew(0deg, 0deg) scale(1, 1); opacity: ${1 * progress};`
-        })
-      },
-      onComplete: () => {
-        [el.i, el.l, el.o, el.v, el.e, el.y, el.o2, el.u].forEach(el => {
-          el.style.opacity = 1;
-          el.style = 'transform: translate(0px, 0px) rotate(0deg) skew(0deg, 0deg) scale(1, 1); opacity: 1;'
-        })
-      }
-    }),
-    
-    new mojs.Html({
-      ...opts,
-      el: el.lineLeft,
-      x: { 0 : 52 },
-    }).then({
-      duration: boom + move,
-      easing,
-      x: { to : 52 + 54 }
-    }).then({
-      duration: boom + move,
-      easing,
-      x: { to : 52 + 54 + 60 }
-    }).then({
-      duration: 150, // 3550
-      easing,
-      x: { to : 52 + 54 + 60 + 10 }
-    }).then({
-      duration: 300
-    }).then({
-      duration: 350,
-      x: { to : 0 },
-      easing: easingOut
-    }),
-    
-    new mojs.Html({
-      ...opts,
-      el: el.lineRight,
-      x: { 0 : -52 },
-    }).then({
-      duration: boom + move,
-      easing,
-      x: { to : -52 - 54 }
-    }).then({
-      duration: boom + move,
-      easing,
-      x: { to : -52 - 54 - 60 }
-    }).then({
-      duration: 150,
-      easing,
-      x: { to : -52 - 54 - 60 - 10 }
-    }).then({
-      duration: 300
-    }).then({
-      duration: 350,
-      x: { to : 0 },
-      easing: easingOut,
-    }),
-    
-    new mojs.Html({ // [I] LOVE YOU
-      ...opts,
-      el: el.i,
-      x: { 0 : 34 },
-    }).then({
-      duration: boom,
-      easing: easingBoom,
-      x: { to : 34 + 19 }
-    }).then({
-      duration: move,
-      easing,
-      x: { to : 34 + 19 + 40 }
-    }).then({
-      duration: boom,
-      easing: easingBoom,
-      x: { to : 34 + 19 + 40 + 30 }
-    }).then({
-      duration: move,
-      easing,
-      x: { to : 34 + 19 + 40 + 30 + 30 }
-    }),
-    
-    new mojs.Html({ // I [L]OVE YOU
-      ...opts,
-      el: el.l,
-      x: { 0 : 15 },
-    }),
-    
-    new mojs.Html({ // I L[O]VE YOU
-      ...opts,
-      el: el.o,
-      x: { 0 : 11 },
-    }),
-    
-    new mojs.Html({ // I LO[V]E YOU
-      ...opts,
-      el: el.v,
-      x: { 0 : 3 },
-    }),
-    
-    new mojs.Html({ // I LOV[E] YOU
-      ...opts,
-      el: el.e,
-      x: { 0 : -3 },
-    }),
-    
-    new mojs.Html({ // I LOVE [Y]OU
-      ...opts,
-      el: el.y,
-      x: { 0 : -20 },
-    }).then({
-      duration: boom,
-      easing: easingBoom,
-      x: { to : -20 - 33}
-    }).then({
-      duration: move,
-      easing,
-      x: { to : -20 - 33 - 24 }
-    }),
-    
-    new mojs.Html({ // I LOVE Y[O]U
-      ...opts,
-      el: el.o2,
-      x: { 0 : -27 },
-    }).then({
-      duration: boom,
-      easing: easingBoom,
-      x: { to : -27 - 27}
-    }).then({
-      duration: move,
-      easing,
-      x: { to : -27 - 27 - 30 }
-    }),
-    
-    new mojs.Html({ // I LOVE YO[U]
-      ...opts,
-      el: el.u,
-      x: { 0 : -32 },
-    }).then({
-      duration: boom,
-      easing: easingBoom,
-      x: { to : -32 - 21}
-    }).then({
-      duration: move,
-      easing,
-      x: { to : -32 - 21 - 36 }
-    }).then({
-      duration: boom,
-      easing: easingBoom,
-      x: { to : -32 - 21 - 36 - 31 }
-    }).then({
-      duration: move,
-      easing,
-      x: { to : -32 - 21 - 36 - 31 - 27 }
-    }),
-    
-    new mojs.Shape({
-      parent: el.container,
-      shape: 'heart',
-      delay: move,
-      fill: el.colHeart,
-      x: -64,
-      scale: { 0 : 0.95, easing: easingHeart },
-      duration: 500
-    }).then({
-      x: { to : -62, easing },
-      scale: { to : 0.65, easing },
-      duration: boom + move - 500,
-    }).then({
-      duration: boom - 50,
-      x: { to: -62 + 48 },
-      scale: { to : 0.90 },
-      easing: easingBoom
-    }).then({
-      duration:  125,
-      scale: { to : 0.8 },
-      easing: easingOut
-    }).then({
-      duration:  125,
-      scale: { to : 0.85 },
-      easing: easingOut
-    }).then({
-      duration: move - 200,
-      scale: { to : 0.45 },
-      easing
-    }).then({
-      delay: -75,
-      duration: 150,
-      x: { to: 0 },
-      scale: { to : 0.90 },
-      easing: easingBoom
-    }).then({
-      duration:  125,
-      scale: { to : 0.8 },
-      easing: easingOut
-    }).then({
-      duration:  125, // 3725
-      scale: { to : 0.85 },
-      easing: easingOut
-    }).then({
-      duration: 125, // 3850
-    }).then({
-      duration: 350,
-      scale: { to : 0 },
-      easing: easingOut
-    }),
-    
-    ...crtBoom(move, -64, 46),
-    ...crtBoom(move * 2 + boom, 18, 34),
-    ...crtBoom(move * 3 + boom * 2 - delta, -64, 34),
-    ...crtBoom(move * 3 + boom * 2, 45, 34)
-  ]);
-};
-
-const loveTl = crtLoveTl().play();
-setInterval(() => { loveTl.replay() }, 4300);
-
-const volume = 0.2;
-el.blup.volume = volume;
-el.blop.volume = volume;
-
-const toggleSound = () => {
-  let on = true;
-  return () => {
-    if (on) {
-      el.blup.volume = 0.0;
-      el.blop.volume = 0.0;
-      el.sound.classList.add('sound--off')
-    }
-    else {
-      el.blup.volume = volume;
-      el.blop.volume = volume;
-      el.sound.classList.remove('sound--off')
-    }
-    on = !on;
-  }
-}
-el.sound.addEventListener('click', toggleSound());
